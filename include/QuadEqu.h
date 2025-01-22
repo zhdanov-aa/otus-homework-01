e5c62fa60454e519cfc79d600f7db56e491de308
zhdanov-aa <zhdanov_aa@mail.ru>
2025-01-22 11:37:18 +0400

    Добавлен прототип SolveQuadEqu

---
+++ include/QuadEqu.h
@@ -0,0 +1,8 @@
+#ifndef __QUADEQU_H_INCLUDED__
+#define __QUADEQU_H_INCLUDED__
+
+#include <vector>
+
+extern std::vector<double> SolveQuadEqu(float a, flota b, float c); 
+
+#endif
---
+++ source/QuadEqu.cpp
@@ -0,0 +1,6 @@
+#include <QuadEqu.h>
+
+std::vector<double> SolveQuadEqu(float a, flota b, float c)
+{
+
+}
